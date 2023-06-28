c1845b8caf445262fc5a14bfc5fd717c8412dff5
seanj29 <88255342+seanj29@users.noreply.github.com>
2023-06-28 16:34:24 +0100

    New version of api request

--- Request.js
+++ Request.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const apiurl = "https://spaces.nexudus.com/api";
+const apiurl = "https://xyz.spaces.nexudus.com/en?_depth=1";
 const username = prompt("Username");
 const password = prompt("Password");
 const auth = "Basic " + Buffer.from(username + ":" + password, "base64");
@@ -43,72 +43,6 @@ async function checkProductExists(product) {
     return false;
 }
 
-async function locateCustomerByEmail(email) {
-    const url =
-        apiurl +
-        "/spaces/coworkers?Coworker_Email=" +
-        encodeURIComponent(email);
-    const response = await axios.get(url, options);
-    if (response.data.Records?.length) {
-        return response.data.Records[0];
-    }
-
-    return false;
-}
-
-async function createProduct(product) {
-    const url = apiurl + "/billing/products";
-    const data = product;
-    const response = await axios.post(url, data, options);
-    return response.data.Value;
-}
-
-async function sellProduct(ProductNo, CoworkerNo) {
-    const url = apiurl + "/billing/coworkerproducts";
-    const data = {
-        BusinessId: 1414974955,
-        CoworkerId: CoworkerNo,
-        ProductId: ProductNo,
-        Quantity: 1,
-        RepeatCycle: "PricePlan",
-    };
-    const response = await axios.post(url, data, options);
-    return response.status;
-}
-
-async function locateAndSell(ProductId) {
-    const EmailExists = await locateCustomerByEmail(CustomerEmail);
-    if (EmailExists) {
-        const CoworkerNo = await EmailExists.UserId;
-        console.log(CoworkerNo);
-        const ServerResponse = await sellProduct(ProductId, CoworkerNo).catch(
-            (err) => {
-                console.error(err.message);
-            }
-        );
-        return ServerResponse;
-    } else {
-        throw new Error("Cannot locate specified customer email");
-    }
-}
-
-async function ProductWrapper() {
-    const ProductExists = await checkProductExists(ProdData);
-    if (ProductExists) {
-        const ProductIdNumber = await ProductExists.Id;
-        console.log(ProductIdNumber);
-        console.log("Product Already Existed");
-        return ProductIdNumber;
-    } else {
-        const CreatedProduct = await createProduct(ProdData).catch((err) => {
-            console.error(err.message);
-        });
-        const ProductIdNumber = CreatedProduct.Id;
-        console.log(ProductIdNumber);
-        console.log("Product Needed to be created");
-        return ProductIdNumber;
-    }
-}
 async function main() {
     const Id = await ProductWrapper();
     const response = await locateAndSell(Id);
