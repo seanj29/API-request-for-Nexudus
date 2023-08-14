3be3f90328b14abd8b756f2324d1ccc05410169e
seanj29 <88255342+seanj29@users.noreply.github.com>
2023-08-14 18:13:49 +0100

    Rethe keymove

--- Request.js
+++ Request.js
@@ -2,17 +2,17 @@ import axios from "axios";
 
 async function testspace() {
     
-    const url = "https://spaces.nexudus.com/api/apps/marketplaceApplications/feed?marketplace_id=f29c7517-543f-4aa2-a17b-2de4b24b9869n";
+    const url = "https://spaces.nexudus.com/api/apps/marketplaceApplications/feed?marketplace_id=f29c7517-543f-4aa2-a17b-2de4b24b9869";
     const headers = {
         accept: 'application/json',
-        authorization: 'Bearer NDMN8z-my_6Sl8UT4kg9UdY2c3k8s8I6'
+        authorization: ///
       }
 
     const response = await axios
     .get(url, headers)
     .catch(err => {
 
-    console.error(err.info)})
+    console.log(err)})
 
     .then((response) =>{
 
