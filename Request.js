672276ae7a778965a9172132c93f5efa6268701b
seanj29 <88255342+seanj29@users.noreply.github.com>
2023-08-15 15:44:28 +0100

    Misspelled.

--- Request.js
+++ Request.js
@@ -1,12 +1,12 @@
 import axios from "axios";
-import 'dotenv/config'
+import 'dotenv/config';
 
 async function testspace() {
     
     const url = "https://spaces.nexudus.com/api/apps/marketplaceApplications/feed?marketplace_id=f29c7517-543f-4aa2-a17b-2de4b24b9869";
     const headers = {
         accept: 'application/json',
-        authorization: process.env.API_KEY
+        authorization: process.env.API_TOKEN
       }
 
     const response = await axios
